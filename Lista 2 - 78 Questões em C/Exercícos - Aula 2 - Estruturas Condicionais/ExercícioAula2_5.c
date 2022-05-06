30b80ba537e4521a95feb967c2f46ec761806953
DarkSancloud <56140684+DarkSan-cloud@users.noreply.github.com>
2022-05-06 00:17:12 +0000

    Exercíco 6 estrutura condicional

---
+++ Lista 2 - 78 Questões em C/Exercícos - Aula 2 - Estruturas Condicionais/ExercícioAula2_5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+  float n1, n2,n3, menor, maior;
+
+  printf("\n Insira tres valores\n\n");
+  scanf("%f%f%f",&n1,&n2,&n3);
+
+  menor = a
+  maior = a
+    
+    if(menor > b)
+        menor = b;
+    if(menor > c)
+        menor = c;
+
+    if(maior < b)
+        maior = b;
+    if(maior < c)
+        maior = c;
+
+    printf("Menor: %f\tMaior: %f\n", menor, maior);
+}
---
+++ Lista 2 - 78 Questões em C/Exercícos - Aula 2 - Estruturas Condicionais/ExercícoAula2_6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  double n1, n2;
+  char op;
+
+
+  printf("\nInsira  um valor\n");
+  scanf("%lf",&n1);
+
+
+  printf("\nInsira outro valor\n");
+  scanf("%lf",&n2);
+
+  printf("Agora insira o simbolo da operacao que voce ira realizar");
+  scanf(" %c",&op);
+
+  switch(op){
+
+    case '+':
+        printf(" SOMA = %f",n1+n2);
+        break;
+
+    case '-':
+        printf("SUBTRACAO = &f",n1-n2);
+        break;
+        
+    case '*':
+        printf("MULTIPLICACAO = %f",n1*n2);
+        break;
+    
+    case '/':
+        print("DIVISAO = %f",n1/n2);
+        break;
+}
+}
