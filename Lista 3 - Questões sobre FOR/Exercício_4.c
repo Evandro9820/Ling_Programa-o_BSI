9cc785dc72b936879aa0cf0fd9126bab6b6ed90c
DarkSancloud <56140684+DarkSan-cloud@users.noreply.github.com>
2022-05-19 23:34:55 +0000

    nsdjvafda

---
+++ Lista 3 - Questões sobre FOR/Exercício_4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+	for (int i = 100; i <= 200; i++) {
+		printf(" %d", i);
+	}
+}
---
+++ Lista 3 - Questões sobre FOR/Exercício_5.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+	for (int i = 200; i <= 100; i--) {
+		printf(" %d", i);
+	}
+}
