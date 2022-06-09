5d91071547e1e52db34122dff563f61ccbb5051d
darksan-cloud <evancavaraujo3@gmail.com>
2022-06-09 20:33:09 -0300

    as

---
+++ Prova em C/Questão 23.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int qtd_lados;
+    float lados, area;
+
+    printf("Insira a quantidade de lados do Poligno: \n");
+    scanf("%d", &qtd_lados);
+
+    // printf("Qual o tamanha dos lados: \n");
+    // scanf("%f", &lados);
+
+    switch (qtd_lados)
+    {
+    case (3):
+        printf("Qual o tamanho dos lados: \n");
+        scanf("%f", &lados);
+        printf("A área desse polígono é: %2f", (lados * lados * .73) / 4);
+        break;
+    case (4):
+        printf("Qual o tamanho dos lados: \n");
+        scanf("%f", &lados);
+        printf("A area desse poligno é: %2f", (lados * lados));
+
+    case (6):
+        printf("Qual o tamanho dos lados: \n");
+        scanf("%f", &lados);
+        printf("A área desse polígono é: %2f", (6 * lados * lados * 1.73) / 4);
+
+    default:    
+        printf("Não sei calcular a area\n\n");
+    }
+}
